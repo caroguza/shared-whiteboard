77057e77c750ba8a9accb4ef8d3193d47908551b
Max <max@monadical.com>
2019-06-06 14:24:24 -0400

    interview!

--- board/consumers.py
+++ board/consumers.py
@@ -2,11 +2,12 @@ import json
 
 from channels.generic.websocket import WebsocketConsumer
 
-from board.models import Stroke
+from board.models import Stroke, Image
 
 
 class BoardConsumer(WebsocketConsumer):
     def connect(self):
+        self.board_group_name = 'board'
         self.accept()
 
     def disconnect(self, close_code):
@@ -23,20 +24,56 @@ class BoardConsumer(WebsocketConsumer):
         elif action == 'draw':
             coordenates = text_data_json['coordenates']
             color = text_data_json['color']
-            self.save_coordenates(username, coordenates, color)
+            self.save_coordinates(username, coordenates, color)
+        elif action == 'save':
+            image_name = text_data_json['image_name']
+            self.save_image(username, image_name)
+        elif action == 'load':
+            image_name = text_data_json['image_name']
+            self.load_image(username, image_name)
 
+    def save_image(self, username, image_name):
+        image = Image.objects.create(name=image_name)
+        image.save()
+        print(f'saved image with name {image_name}')
+        Stroke.objects.filter(onscreen=True).update(image=image)
 
-    def save_coordenates(self, username, coordenates, color):
-        for coordenate in coordenates:
+    def load_image(self, username, image_name):
+        self.clear_board(username)
+        image = Image.objects.get(name=image_name)
+        strokes = Stroke.objects.filter(image=image)
+        strokes.update(onscreen=True)
+        print(f'loading image with name {image_name}')
+        self.send_strokes(strokes)
+
+    def save_coordinates(self, username, coordinates, color):
+        for coordinate in coordinates:
             Stroke.objects.create(
                 username=username, 
-                prev_x=coordenate['prevX'], 
-                prev_y=coordenate['prevY'], 
-                coordenate_x=coordenate['x'],
-                coordenate_y=coordenate['y'], 
-                is_point=coordenate['is_point'], 
+                prev_x=coordinate['prevX'], 
+                prev_y=coordinate['prevY'], 
+                coordenate_x=coordinate['x'],
+                coordenate_y=coordinate['y'], 
+                is_point=coordinate['is_point'], 
                 color=color
             )
                 
     def clear_board(self, username):
-        Stroke.objects.filter(username=username).delete()
+        strokes = Stroke.objects.filter(username=username)
+        strokes.update(onscreen=False)
+
+    def send_strokes(self, strokes):
+        message = json.dumps({
+            'strokes': [
+                {
+                    'prev_x': stroke.prev_x,
+                    'prev_y': stroke.prev_y,
+                    'coordenate_x': stroke.coordenate_x,
+                    'coordenate_y': stroke.coordenate_y,
+                    'is_point': stroke.is_point,
+                    'color': stroke.color,
+                }
+                for stroke in strokes
+            ]
+        })
+        self.send(message)
--- board/models.py
+++ board/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+class Image(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class Stroke(models.Model):
     username = models.CharField(max_length=225)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -11,13 +15,18 @@ class Stroke(models.Model):
     is_point = models.IntegerField(default=0)
     color = models.CharField(max_length=225, default='black')
 
+    onscreen = models.BooleanField(default=True)
+
+    image = models.ForeignKey(Image, on_delete=models.SET_NULL, null=True)
+
     def __str__(self):
         return self.username
 
     @classmethod
     def get_user_strokes(cls, username):
         strokes = cls.objects.filter(
-            username=username
+            username=username,
+            onscreen=True,
         ).values(
             'coordenate_x', 
             'coordenate_y', 
